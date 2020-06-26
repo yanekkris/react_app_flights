e4e82e3dc9a64ddad085783d26f62096170bf3a5
Anna Koriphadze <anakoripadze@yahoo.com>
2020-06-26 15:50:46 +0400

    started second milestone

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import Flight from './Flight';
+import { DateTime } from 'luxon';
 
 const url = 'https://api.skypicker.com/flights?fly_from=CZ&fly_to=VLC&date_from=08/08/2020&date_to=08/09/2020&partner=picky&limit=5'
 
@@ -32,16 +33,49 @@ function Home () {
         <div>
             <h1>Welcome</h1>
             <button>Button!</button>
-            {flights.map((flight, index) => (
-                <Flight key={index} 
-                flight={cityFrom} 
 
-                />, 
-                console.log(flight)
-            ))}
+
+
+
+            {flights ? (
+          flights.map((result, index) => (
+            <div key={index}>
+            <h2>{result.cityFrom} - {result.cityTo}</h2>
+            <p>Departure time: { DateTime.fromMillis(result.dTime * 1000).toFormat('hh:mm') }</p>
+            <p>Arrival time: { DateTime.fromMillis(result.aTime * 1000).toFormat('hh:mm') }</p>
+            <p>Price: { result.price }</p>
+            </div>
+          ))
+        ) : (
+          <h1> Add search results here.</h1>
+        )}
+
+        {loading ? <LoadingSpinner /> : <ResultsTable results={data} />}
             
         </div>
     )
 }
 
 export default Home;
+
+
+
+// const SearchResults = ({ searchResults, handleNextPageClick }) => {
+//     console.log(searchResults);
+//     return (
+//       <div className="searchResults-container">
+//         {flights.length ? (
+//           searchResults.map((result, index) => (
+//             <h2 key={index}>{result.title}</h2>
+//           ))
+//         ) : (
+//           <h1> Add search results here.</h1>
+//         )}
+//         {/**button to update current Offset and load more data */}
+//         <button onClick={handleNextPageClick}>Next Page</button>
+//       </div>
+//     );
+//   };
+  
+//   export default SearchResults;
+  
