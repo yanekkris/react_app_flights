23d8fc561d13b89d99223033a74e45c12ec40065
Kristína Janeková <yanekkris@gmail.com>
2020-06-26 12:54:31 +0100

    Changes

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,21 +3,21 @@ import Flight from './Flight';
 
 const url = 'https://api.skypicker.com/flights?fly_from=CZ&fly_to=VLC&date_from=08/08/2020&date_to=08/09/2020&partner=picky&limit=5'
 
-function Home () {
+function Home() {
     const [searchValue, setSearchValue] = useState([]);
 
     const [flights, setFlights] = useState([]);
-    
 
-    async function fetchData (url) {
+
+    async function fetchData(url) {
         const response = await fetch(url)
         const data = await response.json()
         console.log(data)
         if (data) {
             setFlights(data.data);
-          } else {
+        } else {
             return null;
-          }
+        }
 
         // console.log(data.data[0].cityFrom, data.data[0].cityTo, data.data[0].price, data.data[0].dTime, data.data[0].fly_duration)
         // return data
@@ -33,13 +33,13 @@ function Home () {
             <h1>Welcome</h1>
             <button>Button!</button>
             {flights.map((flight, index) => (
-                <Flight key={index} 
-                flight={cityFrom} 
+                <Flight key={index}
+                    flight={cityFrom}
 
-                />, 
+                />,
                 console.log(flight)
             ))}
-            
+
         </div>
     )
 }
